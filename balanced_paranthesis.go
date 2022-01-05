bc6482f3db19f2ac5149582d5798742a7e36d3cf
Priyadharsshini <dharsshinisakrapani@gmail.com>
2022-01-05 21:33:36 +0530

    Create balanced_paranthesis.go

---
+++ balanced_paranthesis.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+var a []string
+
+func paranthesisChecker(s string) {
+	if len(s)/2 != 0 {
+		fmt.Println("Invalid")
+		return
+	}
+	valid := true
+	for _, val := range s {
+		if string(val) == "(" || string(val) == "{" || string(val) == "[" {
+			a = append(a, string(val))
+		} else if string(val) == ")" && a[len(a)-1] == "(" {
+			a = a[0 : len(a)-1]
+		} else if string(val) == "}" && a[len(a)-1] == "{" {
+			a = a[0 : len(a)-1]
+		} else if string(val) == "]" && a[len(a)-1] == "[" {
+			a = a[0 : len(a)-1]
+		} else {
+			valid = false
+			break
+		}
+	}
+	if valid == true {
+		fmt.Println("Yes valid paranthesis")
+	} else {
+		fmt.Println("Invalid")
+	}
+
+}
+
+func main() {
+  s := "{}[]()"
+	paranthesisChecker(s)
+
+}
